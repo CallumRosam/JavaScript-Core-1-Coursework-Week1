e621feeca72ec8ef72185d48b3a1f4eff0e62dcd
CallumRosam <104946983+CallumRosam@users.noreply.github.com>
2022-08-05 17:46:41 +0100

    1st update

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+let age = 32;
+console.log("Hello Callum! My age is " + age);
+age += 1;
+console.log("Hello Callum! My age is " + age);
+console.log(10);
+console.log("10");
+
